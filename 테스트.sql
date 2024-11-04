298e268bb9c41b4c60b90aa44550ad0083789307
YooUisun <yoouisun33@naver.com>
2024-11-04 17:08:54 +0900

    단일행 함수 -1

---
+++ 테스트.sql
@@ -0,0 +1,12 @@
+select deptno AS "부서번호",
+       dname AS "부서이름",
+       loc AS "위치"
+from dept;
+
+
+select name AS "교수정보",
+       position AS "POSITION",
+       HIREDATE AS "입사일자",
+       Email AS "이메일"
+from professor;
+
