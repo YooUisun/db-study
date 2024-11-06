00b2732c89573778dd179ff66609d943be7a19ff
YooUisun <yoouisun33@naver.com>
2024-11-06 16:59:16 +0900

    북수행 함수-2

--- select03.sql
+++ select03.sql
@@ -38,76 +38,124 @@ select
 --    NVL(height,130)
 from student;
 
+select...
+from...
+where...
+group by
+having
+order by
+
+select deptno, avg(sal)
+from emp
+--where sal >= 2000
+group by deptno
+having AVG(sal) >= 2000;
 
 
--- student 평균키를 구하고 싶은데 1학년,2학년 따로따로의 평균키를 구하고 싶음.
---학년별 평균키를 구하고 싶다.
+1) 급여가 1500 이상인 직원들만 대상으로, 부서별 평균 급여 계산
 
---GROUP BY 특정 컬럼을 기준으로 그룹지어서 계산
---GROUP BY 에 명시된 컬럼은 같이 조회 가능하다.
-SELECT 
-        CASE 
-           WHEN grade = 1 THEN '1학년'
-           WHEN grade = 2 THEN '2학년'
-           WHEN grade = 3 THEN '3학년'
-           WHEN grade = 4 THEN '4학년'
-       END AS 학년,
-       AVG(height) AS 평균키
-FROM student
-GROUP BY grade;
+select deptno, AVG(sal)
+from emp
+where sal >= 1500
+group by deptno;
+2) 부서별 평균 급여가 1500 이상인 경우만, 부서별 평균 급여 보여주기
 
-select grade, avg(height) height
+select deptno, avg(sal)
+from emp
+group by deptno
+having avg(sal) >= 1500
+;
+
+------
+student 각 학년별 평균 몸무게
+평균 몸무게 65키로 이상인 경우만 확인
+
+select grade, avg(weight)
 from student
 group by grade
-order by(height);
+having avg(weight) >= 65
+;
+--
+student 4 학년을 제외하고
+각 학년별 평균 몸무게
+평균 몸무게 65키로 이상인 경우만 확인
 
+select grade, avg(weight)
+from student
+where grade <>4
+group by grade
+having avg (weight) >=65
+;
 
---EMP 전체 SAL 급여 평균
-
-select 
-    avg(sal) as 급여
-from emp;
-
---EMP 30번 부서 사람들 평균급여
-
-select 
-    avg(sal) as 급여
+select deptno,job ,ROUND (avg(sal), 1)
 from emp
-where deptno = 30;
+group by deptno, job; 
 
---EMP 각 부서별 평균 급여
-SELECT 
-    deptno AS 부서번호,
-    AVG(sal) AS 평균급여
-FROM emp
-GROUP BY deptno;
-
---EMP 각 부서별 인원과 평균 급여
-SELECT 
-    deptno AS 부서번호,
-    COUNT(*) AS 인원수,
-    AVG(sal) AS 평균급여
-FROM emp
-GROUP BY deptno
-ORDER BY DEPTNO;        --숫자대로 정렬 시키고 싶으면 ORDER BY 하기
+select deptno,job ,ROUND (avg(sal), 1)
+from emp
+group by ROLLUP (deptno, job); 
 
 
---EMP 각 직업별 평균 급여
-SELECT 
-    job AS 직업,
-    AVG(sal) AS 평균급여
-FROM emp
-GROUP BY job;
---EMP 각 직업별 가장 높은 급여 얼마?
-SELECT  
-    job AS 직업,
-    MAX(sal) AS 높은급여
-FROM emp
-GROUP BY job;
---EMP 각 부서별 가장 높은 급여 얼마?
-SELECT 
-    deptno AS 부서번호,
-    MAX(sal) AS 높은급여
-FROM emp
-GROUP BY deptno
-ORDER BY deptno;
+select *
+from emp
+where job = 'SALESMAN' AND deptno =30;
+
+GROUP BY ROLLUP(deptno, job);
+1) deptno, job 그룹화
+2) deptno 그룹화 계를 냄
+3) ()그룹화 계를 냄
+
+group by ROLLUP( ( deptno, job) );
+1) (deptno, job) 그룹화
+2) ()그룹화 계
+
+select deptno, job ,ROUND(AVG(sal), 1)
+from emp
+group by ROLLUP( (deptno, job) ) ;
+
+GROUP BY ROLLUP (deptno, job, mgr);
+1) deptno, job, mgr 그룹
+2) deptno, job 계
+3) deptno 계
+4) () 계
+GROUP BY ROLLUP (deptno, (job, mgr ) );
+ 1) deptno, job ,mgr 그룹
+ 2) deptno 계
+ 3) () 계
+ 
+ 
+select deptno, job , mgr, ROUND(avg(sal), 1)
+from emp
+group by rollup (deptno, job, mgr);
+
+select deptno, job, mgr, ROUND(AVG(sal), 1)
+from emp
+group by ROLLUP(deptno, job, mgr);
+
+
+
+-- RANK DENSE RANK 순위를 메겨보자
+-- 급여가 높을수록 1위인 차트를 만들어보자.
+
+순위 RANK() OVER (order by 정렬기준)
+그룹단위로 순위를 나타내고 싶을때는 RANK() OVER (PARTITION BY 기준 order by 정렬기준)
+                              --             group by 
+SELECT ename,
+       sal, 
+       RANK() OVER (ORDER BY sal desc),
+       DENSE_RANK() OVER (ORDER BY sal desc)
+FROM emp;
+--order by sal;
+
+--RANK 는 공동 6위가 있으면 그 다음엔 8위로 되는데
+-- DENSE_RANK는 공동 6위가 있으면 그 다음은 7위가 된다.
+SELECT
+        name,
+        height,
+        RANK() OVER (order by height desc) 순위1,
+        DENSE_RANK() OVER (order by height desc) 순위2,
+        grade,
+        RANK() OVER (partition by grade order by height desc) 순위3,
+        DENSE_RANK() OVER (partition by grade order by height desc) 순위4
+FROM student
+order by grade, height desc;
