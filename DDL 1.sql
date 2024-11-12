95df675664e78c3c47fa7856940cf21674616a49
YooUisun <yoouisun33@naver.com>
2024-11-12 17:29:37 +0900

    DDL, DML 연습

--- DDL 1.sql
+++ DDL 1.sql
@@ -1,10 +1,11 @@
--- DDL (테이블 만들기)
+--ddl
 
+--생성
 CREATE TABLE new_table
 (
     no NUMBER(3),
     name VARCHAR2(10),
-    birth DATE 
+    birth DATE
 );
 
 select *
@@ -14,44 +15,44 @@ desc new_table;
 
 select SYSDATE from dual;
 
-create table tt02
+
+CREATE TABLE tt02
 (
-    no Number(3,1) DEFAULT 0,
+    no  NUMBER(3,1) DEFAULT 0,
     name VARCHAR2(10) DEFAULT 'NO Name',
     hiredate DATE DEFAULT SYSDATE
-    
 );
 
 select *
 from tt02;
 
+
 --테이블 복사
+CREATE TABLE dept3
+AS
+select * from dept2;        --구조 + 데이터 전체 복사
 
-Create table dept3
-as
-select * from dept2;            --구조 + 데이터 전체 복사
+select * from dept3;
 
-Create table dept4
-as
-select dcode, area from dept2; --컬럼 2개 +데이터복사
+CREATE TABLE dept4
+AS
+select dcode, area from dept2;  -- 컬럼 2개 + 데이터 복사
 
 select * from dept4;
 
-CREATE TABLE dept5 --컬럼구조만 복사(데이터x)
-as
+CREATE TABLE dept5   -- 컬럼구조만 복사 (데이터 x)
+AS
 select *
 from dept2
-where 1=2; --거짓 조건
+where 1=2;  --거짓 조건
 
-
-select *
-from dept5;
+select * from dept5;
 
 --테이블 변경
 
 --컬럼 추가
 ALTER TABLE dept4
-Add (location VARCHAR2(10));
+ADD (location VARCHAR2(10));
 
 ALTER TABLE dept4
 ADD (location VARCHAR2(10) DEFAULT 'Cheonan');
@@ -59,24 +60,30 @@ ADD (location VARCHAR2(10) DEFAULT 'Cheonan');
 INSERT INTO dept4
 VALUES (2000, 'Cheonan Office');
 
---임시부서 여부를 체크 컬럼명  temp_code 'y' 임시부서 , 'n' 정식부서
+--임시부서 여부를 체크   컬럼명 temp_code   'Y' 임시부서 , 'N' 정식부서
 ALTER TABLE dept4
 ADD (temp_code VARCHAR2(2) DEFAULT 'N');
+
+select * from dept4;
 
 --컬럼 삭제
 ALTER TABLE dept4
 DROP COLUMN location;
 
 
-create table dept 6
-as
-select * from dept 4;
+CREATE TABLE dept6
+AS
+select * from dept4;
 
 select * from dept5;
 select * from dept6;
 
---테이블 삭제 ( rollback 불가 복구 불가능)
-drop table dept5;
+--테이블 삭제 (rollback 불가 / 자동 commit)
+DROP TABLE dept5;
+
+TRUNCATE TABLE dept6;
 
 
-truncate table dept6;
+
+
+
--- DML1.sql
+++ DML1.sql
@@ -1,42 +1,54 @@
--- DML1
+--dml
 
 --데이터 저장 INSERT
-
 --데이터 수정 UPDATE
-
---데이터삭제 DELETE
-
+--데이터 삭제 DELETE
 --데이터 병합 MERGE
 
-INSERT INTO 테이블명 (컬럼명 ...)
-VALUES (데이터 값...)
+INSERT INTO 테이블명 (컬럼명...)
+VALUES (데이터 값...);
 
-INSERT INTO new_table (no,name,birth)
-VALUES(1, '이름1', SYSDATE);
+INSERT INTO new_table (no, name, birth)
+VALUES (1, '이름1', SYSDATE);
 
 INSERT INTO new_table (name, birth, no)
-VALUES('이름2', SYSDATE, 2);
+VALUES ('이름2', SYSDATE, 2);
 
-INSERT INTO new_table (name,birth,no)
-VALUES('이름3', '2002-02-15', 3);
+INSERT INTO new_table (name, birth, no)
+VALUES ('이름3', '2002-02-15', 3);
 
-INSERT INTO new_table (name,birth,no)
-VALUES('이름4', TO_DATE('2001-10-25'), 4 );
+INSERT INTO new_table (name, birth, no)
+VALUES ('이름4', TO_DATE('2001-10-25'), 4);
 
 --컬럼명 생략
 --전체 컬럼에 저장하는 경우 + 순서
-INSERT INTO new_table
+INSERT INTO new_table 
 VALUES (5, '이름5', SYSDATE);
 
 INSERT INTO new_table (no, name)
 VALUES (6, '이름6');
 
-INSERT INTO new_table
+INSERT INTO new_table 
 VALUES (7, '이름7', null);
 
---날짜 입력할땐 TO_DATE('2002-02-15') 이런식으로
+--여러개를 동시에 삽입(저장) INSERT ALL
+INSERT ALL
+INTO tt02 VALUES (7, '이름7', NULL)
+INTO tt02 VALUES (8, '이름9', NULL)
+INTO tt02 VALUES (9, '이름9', NULL)
+INTO tt02 VALUES (10, '이름10', NULL)
+SELECT * FROM dual;
 
------------------
+INSERT INTO tt02 -- 저장되는 타입은 number 3, varchar2, date
+SELECT 11, '샘플', SYSDATE FROM dual;
+
+insert into tt02
+select deptno, loc, sysdate from dept;
+
+select *
+from tt02;
+
+---
 
 INSERT INTO tt02 (no)
 VALUES (1);
@@ -44,56 +56,67 @@ VALUES (1);
 INSERT INTO tt02 (no, name, hiredate)
 VALUES (2, null, null);
 
--- Update 수정
+
+
+--Update 수정
 
 UPDATE 테이블명
 SET 컬럼명 = 값
-WHERE 조건
---임시부서 여부를 체크 컬럼명  temp_code 'y' 임시부서 , 'n' 정식부서
+WHERE 조건;
+
+--임시부서 여부를 체크   컬럼명 temp_code   'Y' 임시부서 , 'N' 정식부서
 select * from dept4;
 
 UPDATE dept4
-SET temp_code ='N';
+SET temp_code = 'N';
 
 UPDATE dept4
-SET temp_code ='Y'
-Where dcode = 2000;
+SET temp_code = 'Y'
+WHERE dcode = 2000;
 
 select *
 from dept4
-where temp_code ='y';
+where temp_code = 'Y';
 
-create table professor2
-as
-select * from professor;
 
+CREATE TABLE professor2
+AS
+SELECT * FROM professor;
 
 select *
 from professor2
 where position = 'assistant professor';
 
 UPDATE professor2
---SET bonus = 200 -- 200으로 저장
-SET bonus = bonus + 200 --200을 인상
+--SET bonus = 200  --200으로 저장
+SET bonus = bonus + 200  --200을 인상
 where position = 'assistant professor';
 
 commit;
 rollback;
 
 select *
-from dept4;
+from dept4
+where dcode = 2000;
 
 UPDATE dept4
 SET location = 'Asan'
-where dcode =2000;
+where dcode = 2000;
 
 INSERT INTO dept4
 VALUES (2001, 'Asan Office', 'Asan', 'Y');
 
--- 데이터 삭제 delete
+--데이터 삭제 DELETE  (rollback 가능 /자동commit X)
 select * from dept4;
 
-select *
---delete 
-from dept4
-where temp_code = 'Y';
+DELETE FROM dept4;
+
+
+SELECT *
+--DELETE
+FROM dept4
+WHERE temp_code = 'Y';
+
+
+
+
